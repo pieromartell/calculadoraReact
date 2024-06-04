8a09fd90843b862687ec66c0f572f69f186c39d2
Piero Martell Falcon <105953521+Pierom689@users.noreply.github.com>
2024-06-03 23:52:17 -0500

    ArreglandoErrores

---
+++ src/presentation/hooks/useCalculator.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+export const useCalculator = () => {
+    const [number, setNumber] = useState('0')
+
+    const buildNumber = (numberString: string) =>{
+
+        if(number.includes('.') && numberString ==='.') return;
+
+        
+//Punto decimal
+        if(number.startsWith('0') && number.startsWith('-0') ){
+            if(numberString === '.'){
+                return setNumber(number + numberString);
+            }
+        }
+
+        if(numberString === '0' && number.includes('.')){
+            return setNumber(number + numberString);
+        }
+
+
+        setNumber(number +  numberString);
+
+    }
+
+
+
+
+  return {
+    number,
+    buildNumber
+  }
+
+   
+}
--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {Pressable, Text, View} from 'react-native';
 import { colors, styles } from '../../config/theme/app-theme';
 import { CalculatorBuuton } from '../components/CalculatorButon';
+import { useCalculator } from '../hooks/useCalculator';
 
 export const CalculatorScreen = () => {
+const {number,buildNumber} = useCalculator()
+
   return (
     <View style={styles.calculatorContainer}>
         <View style={{paddingHorizontal:30,paddingBottom:20}}>
-            <Text style={styles.mainResult}>15000</Text>
+            <Text style={styles.mainResult}>{number}</Text>
             <Text style={styles.subresult}>15</Text>
         </View>
         
@@ -18,27 +21,27 @@ export const CalculatorScreen = () => {
             <CalculatorBuuton onPress={()=>console.log('/')}  label = "/" blackText color={colors.orange}/>
         </View>
         <View style={styles.row}>
-            <CalculatorBuuton onPress={()=>console.log("7")} label = "7" />
-            <CalculatorBuuton onPress={()=>console.log("8")} label = "8" />
-            <CalculatorBuuton onPress={()=>console.log("9")} label = "9" />
+            <CalculatorBuuton onPress={()=>buildNumber("7")} label = "7" />
+            <CalculatorBuuton onPress={()=>buildNumber("8")} label = "8" />
+            <CalculatorBuuton onPress={()=>buildNumber("9")} label = "9" />
             <CalculatorBuuton onPress={()=>console.log("X")} label = "X" color={colors.orange}/>
         </View>
         <View style={styles.row}>
-            <CalculatorBuuton onPress={()=>console.log("4")} label = "4" />
-            <CalculatorBuuton onPress={()=>console.log("5")} label = "5" />
-            <CalculatorBuuton onPress={()=>console.log("6")} label = "6" />
+            <CalculatorBuuton onPress={()=>buildNumber("4")} label = "4" />
+            <CalculatorBuuton onPress={()=>buildNumber("5")} label = "5" />
+            <CalculatorBuuton onPress={()=>buildNumber("6")} label = "6" />
             <CalculatorBuuton onPress={()=>console.log("-")} label = "-" color={colors.orange}/>
         </View>
         <View style={styles.row}>
-            <CalculatorBuuton onPress={()=>console.log("1")} label = "1" />
-            <CalculatorBuuton onPress={()=>console.log("2")} label = "2" />
-            <CalculatorBuuton onPress={()=>console.log("3")} label = "3" />
+            <CalculatorBuuton onPress={()=>buildNumber("1")} label = "1" />
+            <CalculatorBuuton onPress={()=>buildNumber("2")} label = "2" />
+            <CalculatorBuuton onPress={()=>buildNumber("3")} label = "3" />
             <CalculatorBuuton onPress={()=>console.log("+")} label = "+" color={colors.orange}/>
         </View>
 
         <View style={styles.row}>
-            <CalculatorBuuton onPress={()=>console.log("0")} label = "0" doubleSize/>
-            <CalculatorBuuton onPress={()=>console.log("3")} label = "3" />
+            <CalculatorBuuton onPress={()=>buildNumber("0")} label = "0" doubleSize/>
+            <CalculatorBuuton onPress={()=>buildNumber(".")} label = "." />
             <CalculatorBuuton  onPress={()=>console.log("=")} label = "=" color={colors.orange}/>
         </View>
 
